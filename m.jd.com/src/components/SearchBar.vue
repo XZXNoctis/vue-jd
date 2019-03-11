c778800b52e05670ae46727f50eb933dba3f6e6e
niyinlong <niyinlong@126.com>
2019-03-11 16:36:42 +0800

    修改为弹性布局

--- m.jd.com/src/components/SearchBar.vue
+++ m.jd.com/src/components/SearchBar.vue
@@ -1,12 +1,17 @@
 <template>
 
-	<div class="search-bar">
-		<div class="category"></div>
-		<div class="search-from">
-			<input type="" name="" id="" value="" />
-		
+	<div class="search-bar flex">
+		<div class="category">
+
+			<span class="icon"></span>
 		</div>
-		<div class="login"></div>
+		<div class="search-from  flex-item">
+			<span class="icon"></span>
+			<input type="" name="" id="" value="" />
+		</div>
+		<div class="login">
+			登录
+		</div>
 	</div>
 
 </template>
@@ -26,6 +31,57 @@
 
 <style>
 	.search-bar {
+		height: 44px;
+		background-color: #e43130;
+	}
+	
+	.search-bar .category,
+	.search-bar .login {
+		width: 50px;
+		height: 44px;
+	}
+	
+	.search-bar .category .icon {
+		margin: 14px 0 0 15px;
+		width: 20px;
+		height: 18px;
+		display: block;
+		background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACgAAAAgAgMAAAAdw9KTAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAMUExURUdwTP///////////waf0AoAAAADdFJOUwDjSYAlncUAAAAbSURBVBjTY5j/Hwq+MdTDmH+RmUgK6AuGhcsAU5tyB6Ji+x0AAAAASUVORK5CYII=) no-repeat;
+		background-size: 100% 100%;
+	}
+	
+	.search-bar .login {
+		color: #fff;
+		font-size: 14px;
+		text-align: center;
+		line-height: 44px;
+	}
+	
+	.search-from {
+		padding-top: 7px;
+		position: relative;
+	}
+	
+	.search-from .icon {
+		width: 20px;
+		height: 15px;
+		background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACgAAAAeBAMAAACs80HuAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAwUExURUdwTOk7Puo7PfE+SP9SUuo7Peo8Pu0/Qeo7Peo7Peo7Pek7Peo7Pes8Pus8P+k7PaLYf6IAAAAPdFJOUwCjwREGj1sg9Hrd6bFINxDM6d8AAAD3SURBVCjPY2AAAv3/nxnk////sUo1nQEOoIIgoOaARfD/WmyC/wNQBL+lzZQCCn5FEfwCZJX2//8/AV2Qoej//3YMQWb9/x8xBBkq/v8vwBBk/f8/AUOQ/f9/AQxBhvX/N2AKnv//AVMwHsjBEJz//zs2lVgE5bFpx2pR//8FUMEvcEE+mOPX/2+AC/JAvcnyH6gDJvj+/w8DEM0G0gEVZN8PDfpqkA6o4O3//5tBtGX//98OEEHmJGAkXWBgCNQC0t9Abv4aKrMewmYAR6sAAzyKfydABX8yIASFGSCCjQ/ggr+mg61WOjgXnHwmKSnpiM4FO5EBAIlo6/K7pNY1AAAAAElFTkSuQmCC) no-repeat;
+		background-size: 20px 15px;
+		margin: 8px 8px 0 15px;
+		position: absolute;
+		z-index: 1;
+	}
+	
+	.search-from input {
+		width: 100%;
+		height: 30px;
+		padding-left: 68px;
+		box-sizing: border-box;
+		border-radius: 15px;
+		background-color: #FFFFFF;
+		border: none;
+	}
+	/*.search-bar {
 		height: 44px;
 		background-color: red;
 		position: relative;
@@ -56,6 +112,5 @@
 		box-sizing: border-box;
 		border-radius: 15px;
 		background-color: #FFFFFF;
-	}
-	
+	}*/
 </style>
